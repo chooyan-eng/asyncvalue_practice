5121840cda6b7846d52936d6625012fa98530574
chooyan-eng <chooyan.engineer@gmail.com>
2023-10-05 13:41:47 +0900

    add controll for _skipLoadingOn

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,21 @@ void main() {
   runApp(const ProviderScope(child: _MainApp()));
 }
 
-class _MainApp extends ConsumerWidget {
+class _MainApp extends ConsumerStatefulWidget {
   const _MainApp();
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState createState() {
+    return _MainAppState();
+  }
+}
+
+class _MainAppState extends ConsumerState<_MainApp> {
+  var _skipLoadingOnRefresh = false;
+  var _skipLoadingOnReload = true;
+
+  @override
+  Widget build(BuildContext context) {
     final asyncValue = ref.watch(fetchDataProvider);
     final notifier = ref.watch(fetchDataProvider.notifier);
 
@@ -58,6 +68,8 @@ class _MainApp extends ConsumerWidget {
               _InfoRow(
                 label: '.when',
                 content: asyncValue.when(
+                  skipLoadingOnRefresh: _skipLoadingOnRefresh,
+                  skipLoadingOnReload: _skipLoadingOnReload,
                   data: (data) => 'data -> $data',
                   loading: () => 'loading',
                   error: (e, stack) => 'error -> $e',
@@ -68,21 +80,47 @@ class _MainApp extends ConsumerWidget {
                 label: '.valueOrNull',
                 content: asyncValue.valueOrNull?.toString() ?? 'null',
               ),
+              const SizedBox(height: 32),
+              _CheckRow(
+                label: 'skipLoadingOnRefresh',
+                value: _skipLoadingOnRefresh,
+                onChanged: (value) {
+                  setState(() => _skipLoadingOnRefresh = value);
+                },
+              ),
+              _CheckRow(
+                label: 'skipLoadingOnReload',
+                value: _skipLoadingOnReload,
+                onChanged: (value) {
+                  setState(() => _skipLoadingOnReload = value);
+                },
+              ),
               const SizedBox(height: 60),
-              ElevatedButton(
-                onPressed: () => notifier.updateData(Random().nextInt(100)),
-                child: const Text('state = AsyncData'),
-              ),
-              const SizedBox(height: 4),
-              OutlinedButton(
-                onPressed: () => notifier.updateLoading(),
-                child: const Text('state = AsyncLoading'),
-              ),
-              const SizedBox(height: 4),
-              TextButton(
-                style: TextButton.styleFrom(foregroundColor: Colors.red),
-                onPressed: () => notifier.updateError(),
-                child: const Text('state = AsyncError'),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () =>
+                          notifier.updateData(Random().nextInt(100)),
+                      child: const Text('Data'),
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: () => notifier.updateLoading(),
+                      child: const Text('Loading'),
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  Expanded(
+                    child: TextButton(
+                      style: TextButton.styleFrom(foregroundColor: Colors.red),
+                      onPressed: () => notifier.updateError(),
+                      child: const Text('Error'),
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(height: 4),
               ElevatedButton(
@@ -131,6 +169,37 @@ class _InfoRow extends StatelessWidget {
         Text(
           content,
           style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
+
+class _CheckRow extends StatelessWidget {
+  const _CheckRow({
+    required this.label,
+    required this.value,
+    required this.onChanged,
+  });
+
+  final String label;
+  final bool value;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            label,
+            style: const TextStyle(fontSize: 20),
+          ),
+        ),
+        const SizedBox(width: 16),
+        Checkbox(
+          value: value,
+          onChanged: (value) => onChanged(value ?? false),
         ),
       ],
     );
